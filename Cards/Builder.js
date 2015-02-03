81584b36cba10409036e0a8afee8de8fc3cc36f9
smarryboyina <smarryboyina@telogical.com>
2015-02-03 08:06:08 -0600

    Adding more data

--- Cards/Builder.js
+++ Cards/Builder.js
@@ -1,5 +1,8 @@
 var appNode = document.getElementById('card_example');
+
 var cardStackNode = document.getElementById('card_stack_example');
+
+
 React.render(
     React.createElement(Card, {author: "Sameeri", cardType: "Question", content: "Why do we use Angular?"}), appNode);
 
@@ -13,7 +16,9 @@ React.renderComponent(React.createElement(CardStack,
                 {author: "Benji", cardType: "Question", content: "What testing framework do we use?"},
                 {author: "Rupesh", cardType: "Question", content: "For what kind of testing?"},
                 {author: "Benji", cardType: "Response", content: "Unit testing"},
-                {author: "Rupesh", cardType: "Response", content: "Mocha. We use chai and sinon as helpers."}
+                {author: "Rupesh", cardType: "Response", content: "Mocha. We use chai and sinon as helpers."},
+                {author: "Sameeri", cardType: "Question", content: "Why don't we use Jasmine? Everyone's using Jasmine."},
+                {author: "Ben", cardType: "Response", content: "Mocha is like Jasmine. Except it's newer and better."},
             ]
     }
 ), cardStackNode);
